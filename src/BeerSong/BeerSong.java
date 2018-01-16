10e4bf5e08e6a233fc9f7ca242e664968b3dda3a
xabertum <mohatar@gmail.com>
2018-01-16 15:49:00 +0100

    Code added

--- src/BeerSong/BeerSong.java
+++ src/BeerSong/BeerSong.java
@@ -13,7 +13,13 @@ public class BeerSong {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int beerNum = 99;
+		String word = "bottles";
+		
+		while (beerNum > 0 ) {
+			
+		}
+		
 
 	}
 
