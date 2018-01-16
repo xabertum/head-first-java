0371d343b398d3c84a93df28b2d118b8921a7de6
xabertum <mohatar@gmail.com>
2018-01-16 15:54:54 +0100

    Minor changes

--- src/BeerSong/BeerSong.java
+++ src/BeerSong/BeerSong.java
@@ -23,11 +23,15 @@ public class BeerSong {
 			System.out.println("Pass it around");
 			beerNum = beerNum - 1;
 			
+			if (beerNum > 0) {
+				System.out.println(beerNum + "" + word + " of beer on the wall");
+			} else {
+				System.out.println("No more bottles of beer on the wall");
+			} //end else
 			
-			
-		}
+		} //end while loop
 		
 
-	}
+	} //end main method
 
-}
+} //end Class
