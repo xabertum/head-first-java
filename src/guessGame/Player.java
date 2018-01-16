a6e8c47a2bbaed804d8816af9e4d3ba56e3f57ea
xabertum <mohatar@gmail.com>
2018-01-16 23:57:07 +0100

    Minor changes

--- src/guessGame/Player.java
+++ src/guessGame/Player.java
@@ -5,8 +5,9 @@ public class Player {
 	int number = 0;
 
 	public void guess() {
-		number = (int) Math.random() * 10;
+		number = (int) (Math.random() * 10);
 		System.out.println("I'm guessing: " + number);
+				
 	}
 
 }
