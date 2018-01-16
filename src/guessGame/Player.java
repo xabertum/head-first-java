399175eb8a674524f9b25c528af5cd8786b61457
xabertum <mohatar@gmail.com>
2018-01-16 23:27:46 +0100

    Added Player Class methods

--- src/guessGame/Player.java
+++ src/guessGame/Player.java
@@ -2,4 +2,11 @@ package guessGame;
 
 public class Player {
 
+	int number = 0;
+
+	public void guess() {
+		number = (int) Math.random() * 10;
+		System.out.println("I'm guessing: " + number);
+	}
+
 }
