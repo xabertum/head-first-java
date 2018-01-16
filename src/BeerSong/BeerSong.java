036e86ed9ad81d53b570dfe432ab7736092b4927
xabertum <mohatar@gmail.com>
2018-01-16 15:30:18 +0100

    Initial commit

---
+++ src/BeerSong/BeerSong.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package BeerSong;
+
+/**
+ * @author xabertum
+ *
+ */
+public class BeerSong {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
