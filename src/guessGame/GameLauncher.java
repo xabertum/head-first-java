0593211328605ec6583aafd9eaf5d035a2a60c7b
xabertum <mohatar@gmail.com>
2018-01-16 23:33:45 +0100

    Added Class

---
+++ src/guessGame/GameLauncher.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package guessGame;
+
+/**
+ * @author xabertum
+ *
+ */
+public class GameLauncher {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		GuessGame game = new GuessGame();
+		game.startGame();
+		
+
+	}
+
+}
