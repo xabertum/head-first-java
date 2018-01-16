b17b1026d5c5e27776fdc7cb46941813ce228b6d
xabertum <mohatar@gmail.com>
2018-01-16 19:07:40 +0100

    Final commit

--- src/phraseOMatic/PhraseOMatic.java
+++ src/phraseOMatic/PhraseOMatic.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package phraseOMatic;
 
 /**
@@ -14,7 +11,27 @@ public class PhraseOMatic {
 	 */
 	public static void main(String[] args) {
 
+		String[] wordListOne = { "24/7", "multiTiar", "30,OOO foot", "B-to-B", "win-win", "frontend", "web- based",
+				"pervasive", "smart", "sixsigma", "cri tical-path", "dynamic" };
 
+		String[] wordListTwo = { "empowered", "sticky", "value-added.", "oriented", "centric", "distributed",
+				"clustered", "branded", "outaide-the-box", "positioned", "networked", " f ocused", "leveraged",
+				"aligned", "targeted", "shared", "cooperative", "accelerated" };
+
+		String[] wordListThree = { "process", "tippingpoint", "solution", "architecture", "core competency", "strategy",
+				"mindshare", "portal", "apace", "vision", "paradigm", "mission" };
+
+		int oneLength = wordListOne.length;
+		int twoLength = wordListTwo.length;
+		int threeLenght = wordListThree.length;
+
+		int rand1 = (int) (Math.random() * oneLength);
+		int rand2 = (int) (Math.random() * twoLength);
+		int rand3 = (int) (Math.random() * threeLenght);
+
+		String phrase = wordListOne[rand1] + "" + wordListTwo[rand2] + "" + wordListThree[rand3];
+
+		System.out.println("What we need is a " + phrase);
 
 	}
 
